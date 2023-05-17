e00fce7c239b4730955f8b9ec1e3de8593a4f7f9
Ryan Jiang <jiangr0421@gmail.com>
2023-05-17 16:43:00 -0400

    Changed long links

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -12,21 +12,21 @@ const seedDB = async () => {
 		name: "Hunter College",
 		address: "695 Park Ave, New York, NY 10065",
 		description: "This is a school in New York, New York.",
-		imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FHunter_College&psig=AOvVaw2DCRHSRze2gkdFJPLpvVBN&ust=1684431797841000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjThsrz_P4CFQAAAAAdAAAAABAO",
+		imageUrl: "https://s29068.pcdn.co/wp-content/uploads/hunter-new-york-city-street-view.jpg",
 	});
 	// Create a new campus
 	const dummy_campus2 = await Campus.create({
 		name: "Queens College",
 		address: "65-30 Kissena Blvd, Queens, NY 11367",
 		description: "This is a school in Queens, New York.",
-		imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Fnew-york-queens-college.html&psig=AOvVaw12hF3Y_bPqNXN_KISukX1o&ust=1684431838145000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNDA29zz_P4CFQAAAAAdAAAAABAE",
+		imageUrl: "https://nypost.com/wp-content/uploads/sites/2/2020/03/shutterstock_210220948.jpg?quality=75&strip=all",
 	});
 	// Create a new campus
 	const dummy_campus3 = await Campus.create({
 		name: "Brooklyn College",
 		address: "2900 Bedford Ave, Brooklyn, NY 11210",
 		description: "This is a school in Brooklyn, New York.",
-		imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brooklyn.cuny.edu%2Fweb%2Fabout%2Fcampus%2Fzoom-backgrounds.php&psig=AOvVaw0pqltCrqg7raxYA8JD5SIu&ust=1684431859025000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMjyyebz_P4CFQAAAAAdAAAAABAJ",
+		imageUrl: "https://www.brooklyn.cuny.edu/web/new_2019news/190813_campusshot_960x640.jpg",
 	});
 	
 	// Create a new student for a campus
@@ -34,7 +34,7 @@ const seedDB = async () => {
 		firstname: "Joe",
       	lastname: "Smith",
 		email: "joesmith@gmail.com",
-		imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcprstoronto.com%2F2015%2F01%2F28%2Fstudent-steering-committee-profile-matthew-palmer%2F&psig=AOvVaw1LDR4VFJq9PUP5W4t3P8Ol&ust=1684431915086000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjk_4H0_P4CFQAAAAAdAAAAABAE",
+		imageUrl: "https://t4.ftcdn.net/jpg/00/68/92/17/360_F_68921781_CNvWxI6HCmYKs6DxmKv0KC2jnaI5ll4o.jpg",
 		gpa: 3.5,
 	});
 	// Create a new student for a campus
@@ -42,8 +42,8 @@ const seedDB = async () => {
 		firstname: "Mary",
       	lastname: "Johnson",
 		email: "maryjohnson@gmail.com",
-		imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftucson.com%2Fnews%2Fpolice-university-of-north-carolina-student-leaders-killing-was-random%2Farticle_4b7e8eb3-4d8e-54cb-9b8b-55d0d124bfdb.html&psig=AOvVaw1zIflIhkxb8zNBVAvQ1lxm&ust=1684431934096000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOifgIv0_P4CFQAAAAAdAAAAABAE",
-		gpa: 2.6,
+		imageUrl: "https://www.postgrad.com/filer/canonical/1568839692/6810/",
+		gpa: 2.3,
 	});
 
 	// Add students to campuses
