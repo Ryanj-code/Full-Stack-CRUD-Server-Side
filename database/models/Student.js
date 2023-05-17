e087992f7a28dd5393fdba45a29e4d186eb56665
gnauh <evanacademics@gmail.com>
2023-05-17 13:43:04 -0400

    updated models

--- database/models/Student.js
+++ database/models/Student.js
@@ -9,13 +9,37 @@ const db = require('../db');  // Import Sequelize database instance called "db"
 const Student = db.define("student", {
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate:{
+      isEmail: true
+    }
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+  },
+
+  gpa: {
+    type: Sequelize.FLOAT(),
+    validate:{
+      isWithinRange(value) {
+        if(value < 0.0 || value > 4.0) {
+          throw new Error('gpa must be between 1.0 and 4.0');
+        }
+      }
+    }
   }
+
 });
 
 // Export the student model
